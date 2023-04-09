02eca9f0af544181f37c9e3f758e137e64a73ce6
Zac <zaccrites@gmail.com>
2023-04-09 10:06:28 -0400

    SettingsPanel tests

--- src/PropertySlider.tsx
+++ src/PropertySlider.tsx
@@ -1,4 +1,6 @@
 import "./PropertySlider.css"
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 
 type PropertySliderProps = {
     label: string
@@ -18,17 +20,29 @@ const PropertySlider = ({
     onChange,
 }: PropertySliderProps) => {
 
+    const id = "slider" + uuidv4().toString();
+
+    const [input,setInput] = useState("");
+
+    const handleChange = (v:string) => {
+        if ( !v || input === v )
+            return;
+        setInput(v);
+        onChange(v);
+    }
+
     return (
         <div className="slider">
             <div>
-                <div>{label}</div>
+                <label htmlFor={id}>{label}</label>
                 <input
+                    id={id}
                     type="number"
                     min={min}
                     max={max}
                     value={value}
                     onFocus={(e)=>e.target.select()}
-                    onChange={(e)=>onChange(e.target.value)}
+                    onChange={(e)=>handleChange(e.target.value)}
                 />
             </div>
             <input
@@ -37,7 +51,7 @@ const PropertySlider = ({
                 max={max}
                 value={value}
                 step={step}
-                onChange={(e)=>onChange(e.target.value)}
+                onChange={(e)=>handleChange(e.target.value)}
             />
         </div>
     )
---
+++ src/tests/SettingsPanel.test.tsx
@@ -0,0 +1,63 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SettingsPanel from '../SettingsPanel';
+import { CreateChatCompletionRequest } from 'openai';
+import { act } from 'react-dom/test-utils'
+
+describe('SettingsPanel', () => {
+    let mockRequestSettings: CreateChatCompletionRequest;
+    let mockSetRequestSettings: jest.Mock;
+
+    beforeEach(() => {
+        mockRequestSettings = {
+            model: "mockModel",
+            max_tokens: 200,
+            temperature: 0.5,
+            top_p: 0.8,
+            frequency_penalty: 0.2,
+            presence_penalty: 0.4,
+            messages: []
+        };
+        mockSetRequestSettings = jest.fn();
+    });
+
+    it('renders all sliders with default values', () => {
+        render(
+            <SettingsPanel
+                requestSettings={mockRequestSettings}
+                setRequestSettings={mockSetRequestSettings}
+            />
+        );
+        expect(screen.getByLabelText('Max Tokens:')).toHaveValue(200);
+        expect(screen.getByLabelText('Temperature:')).toHaveValue(0.5);
+        expect(screen.getByLabelText('Top P:')).toHaveValue(0.8);
+        expect(screen.getByLabelText('Frequency Penalty:')).toHaveValue(0.2);
+        expect(screen.getByLabelText('Presence Penalty:')).toHaveValue(0.4);
+    });
+
+it.each`
+sliderName | sliderLabel | newValue
+${'top_p'} | ${'Top P:'} | ${'0.5'}
+${'max_tokens'} | ${'Max Tokens:'} | ${'300'}
+${'temperature'} | ${'Temperature:'} | ${'0.8'}
+${'presence_penalty'} | ${'Presence Penalty:'} | ${'0.7'}
+${'frequency_penalty'} | ${'Frequency Penalty:'} | ${'0.1'}
+`('updates $sliderName slider on change', ({ sliderName, sliderLabel, newValue }) => {
+        render(
+            <SettingsPanel
+                requestSettings={mockRequestSettings}
+                setRequestSettings={mockSetRequestSettings}
+            />
+        );
+        const slider = screen.getByLabelText(sliderLabel) as HTMLInputElement;
+        act(() => {
+            userEvent.click(slider);
+            userEvent.clear(slider);
+            userEvent.paste(slider, newValue);
+        });
+        expect(mockSetRequestSettings).toHaveBeenCalledWith({
+            ...mockRequestSettings,
+            [sliderName]: parseFloat(newValue),
+        });
+    });
+});
